063d18495a9b9648c054bb03b0bcd69693aae7ae
Enzo <enz.dard@gmail.com>
2023-10-02 14:32:47 +0200

    Ecoute background

--- ear.py
+++ ear.py
@@ -1,4 +1,6 @@
 import speech_recognition as sr
+import tts
+import api
 
 def enregistrer_audio():
     r = sr.Recognizer()
@@ -17,3 +19,22 @@ def transcrire_audio(audio):
         print("Impossible de reconnaître le texte.")
     except sr.RequestError as e:
         print("Erreur lors de la demande au service de reconnaissance vocale : {0}".format(e))
+
+def callback(recognizer, audio):                          # this is called from the background thread
+    try:
+        msg = recognizer.recognize_google(audio, language="fr-FR")
+
+        if "la mère de grégor" in msg.lower():
+            print(msg.lower().split("la mère de grégory ")[-1])
+            
+            reponse_chatgpt = api.appeler_api_chatgpt(texte_reconnu)
+            print(reponse_chatgpt)
+            tts.say(reponse_chatgpt)
+
+            pass
+    except LookupError:
+        print("Oops! Didn't catch that")
+
+def ecoute_background():
+    r = sr.Recognizer()
+    r.listen_in_background(sr.Microphone(), callback)
--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import ear
 import api
 import tts
 import os
+import time
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+ear.ecoute_background()
+
 while True:
 
-    audio = ear.enregistrer_audio()
+    """audio = ear.enregistrer_audio()
     texte_reconnu = ear.transcrire_audio(audio)
-    print(texte_reconnu)
+    print(texte_reconnu)"""
 
-    reponse_chatgpt = api.appeler_api_chatgpt(texte_reconnu)
+    time.sleep(0.1)
+    """reponse_chatgpt = api.appeler_api_chatgpt(texte_reconnu)
     print(reponse_chatgpt)
-    tts.say(reponse_chatgpt)
+    tts.say(reponse_chatgpt)"""
