8b2fd0fe46ce6d193184074dc5c0fdb6bff31635
Grégorie Hacquard <gregorie.hacquard@mines-ales.org>
2024-02-23 16:29:02 +0100

    First version front

---
+++ front/islaproject/src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import MainPage from './components/MainPage.vue'
+import MainSection from './components/MainSection.vue'
+import ProductVue from './components/ProductVue.vue'
+document.body.classList.add("dark");
+</script>
+
+
+<template>
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <MainPage msg="You did it!" />
+    </div>
+  </header> -->
+
+    <div id="back-bg"></div>
+    <div id="back-colors" class="relative flex flex-col h-[100vh] items-center justify-center bg-white dark:bg-black transition-bg">
+        <div class="absolute inset-0 overflow-hidden">
+            <div class="jumbo absolute -inset-[10px] opacity-50"></div>
+        </div>
+    </div>
+
+  <MainSection msg="You did it!" />
+  
+  <ProductVue />
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+</style>
---
+++ front/islaproject/src/components/MainSection.vue
@@ -0,0 +1,95 @@
+<template>
+    
+    <div class="container">
+      <h1> Isla </h1>
+      <a class="button-21" role="button" href="https://buy.stripe.com/test_aEU01o3WdaAwbF6dQQ">Acheter</a>
+
+    </div>
+  </template>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh; /* Pour centrer verticalement */
+  }
+  
+  .logo-container {
+    margin-bottom: 20px;
+  }
+  
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+  }
+  
+  h3 {
+    font-size: 1.2rem;
+  }
+  
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+  
+  @media (min-width: 1024px) {
+    .greetings h1,
+    .greetings h3 {
+      text-align: left;
+    }
+  }
+
+/* CSS */
+.button-21 {
+  align-items: center;
+  appearance: none;
+  background-color: #3eb1fdb7;
+  background-image: linear-gradient(1deg, #4F58FD, #149BF3 99%);
+  background-size: calc(100% + 20px) calc(100% + 20px);
+  border-radius: 100px;
+  border-width: 0;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: CircularStd,sans-serif;
+  font-size: 1rem;
+  height: auto;
+  justify-content: center;
+  line-height: 1.5;
+  padding: 6px 20px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color .2s,background-position .2s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+}
+
+.button-21:active,
+.button-21:focus {
+  outline: none;
+}
+
+.button-21:hover {
+  background-position: -20px -20px;
+}
+
+.button-21:focus:not(:active) {
+  box-shadow: rgba(40, 170, 255, 0.25) 0 0 0 .125em;
+}
+  
+  </style>
+  
+
+
+
+  
